feace2a32fd5ec11fcb393a15e711d161a2ad124
Lil-boss <rshaon09@gmail.com>
2022-05-08 08:31:02 +0600

    pagination success

--- src/components/ManageItem/ManageItem.jsx
+++ src/components/ManageItem/ManageItem.jsx
@@ -1,16 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../sidebar/Sidebar';
 import ShowItem from '../ShowItem/ShowItem';
-// import UseProductItem from '../../Hooks/UseProductItem';
+import UseProductItem from '../../Hooks/UseProductItem';
 import toast from 'react-hot-toast';
 import axios from 'axios';
+import Pagination from '../Pagination/Pagination';
 const ManageItem = () => {
-    const [page, setPage] = useState(0);
-    const [pageCount, setPageCount] = useState(0);
-    const [count, setCount] = useState(6);
-    const [products, setProducts] = useState([]);
-
-    // const [products, setProducts] = UseProductItem();
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postPerPage, setPostPerPage] = useState(10);
+    const [products, setProducts] = UseProductItem();
     const deleteItem = (id) => {
         const fetchData = async () => {
             const confirm = window.confirm("Are you sure you want to delete this item?");
@@ -34,59 +32,21 @@ const ManageItem = () => {
         }
         fetchData();
     }
+    const indexOfLastPost = currentPage * postPerPage;
+    const indexOfFirstPost = indexOfLastPost - postPerPage;
+    const currentPosts = products.slice(indexOfFirstPost, indexOfLastPost);
 
-    useEffect(() => {
-        try {
-            axios.get(`https://fruitsroyal.herokuapp.com/api/product?page=${page}&size=${count}`, {
-                headers: {
-                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
-                }
-            })
-                .then(res => { setProducts(res.data) })
-        } catch {
-            toast.error('Something went wrong', { id: "wrong" });
-        }
-    }, [page, count]);
-
-    //countpage
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                await axios.get(`https://fruitsroyal.herokuapp.com/api/product/count`, {
-                    headers: {
-                        authorization: `Bearer ${localStorage.getItem('accessToken')}`
-                    }
-                })
-                    .then(res => setPageCount(Math.ceil(res.data / 6)));
-            } catch {
-                toast.error("Failed to fetch data", { id: "failed" })
-            }
-        }
-        fetchData();
-    }, []);
-
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
     return (
         <div>
             <Sidebar />
             <div className='w-4/5 mx-auto mt-8'>
                 <div className='grid sm:grid-cols-1 md:grid-cols-3 gap-2'>
-                    {products.map(product => { return <ShowItem key={product._id} product={product} deleteItem={deleteItem} /> })}
+                    {currentPosts.map(product => { return <ShowItem key={product._id} product={product} deleteItem={deleteItem} /> })}
                 </div>
             </div>
-            <div className='flex justify-center items-center my-8'>
-                <div className='bg-gray-400'>
-                    {
-                        [...Array(pageCount).keys()].map(number =>
-                            <button className={page === number ? "bg-red-200 ml-4 p-4" : "ml-4 p-4"} onClick={() => setPage(number)}>{number + 1}</button>
-                        )
-                    }
-                    <select onChange={e => setCount(e.target.value)}>
-                        <option value="5">5</option>
-                        <option value="10" selected>10</option>
-                        <option value="15">15</option>
-                        <option value="20">20</option>
-                    </select>
-                </div>
+            <div className='my-4'>
+                <Pagination postPerPage={postPerPage} totalPosts={products.length} paginate={paginate} />
             </div>
         </div>
     );
--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,48 +1,21 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
+import { useState } from "react";
 
-const Pagination = () => {
-    const [page, setPage] = useState(0);
-    const [pageCount, setPageCount] = useState(0);
-    const [count, setCount] = useState(10);
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        try {
-            axios.get(`https://fruitsroyal.herokuapp.com/api/product?page=${page}&size=${count}`, {
-                headers: {
-                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
-                }
-            })
-                .then(res => { setProducts(res.data) })
-        } catch {
-            toast.error('Something went wrong', { id: "wrong" });
-        }
-    }, [page, count]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                await axios.get(`https://fruitsroyal.herokuapp.com/api/product/count`, {
-                    headers: {
-                        authorization: `Bearer ${localStorage.getItem('accessToken')}`
-                    }
-                })
-                    .then(res => setPageCount(Math.ceil(res.data / 6)));
-            } catch {
-                toast.error("Failed to fetch data", { id: "failed" })
-            }
-        }
-        fetchData();
-    }, []);
+const Pagination = ({ postPerPage, totalPosts, paginate }) => {
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(totalPosts / postPerPage); i++) {
+        pageNumbers.push(i);
+    }
+    const [page, setPage] = useState(1);
+    const submit = (id) => {
+        paginate(id);
+        setPage(id);
+    }
     return (
         <div>
             <div className='flex justify-center items-center'>
                 <div className='bg-gray-400'>
                     {
-                        [...Array(pageCount).keys()].map(number =>
-                            <button className={page === number ? "bg-red-200 ml-4 p-4" : "ml-4 p-4"} onClick={() => setPage(number)}>{number + 1}</button>
-                        )
+                        pageNumbers.map(number => <button onClick={() => submit(number)} className={page === number ? "bg-red-100 ml-2 p-4 " : "ml-2 p-4 bg-gray-400"} key={number}>{number}</button>)
                     }
                 </div>
             </div>
